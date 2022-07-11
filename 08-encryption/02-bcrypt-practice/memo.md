e06e1ec89d158302a9117f1601bdf393ecce0c33
SOunit <workbookwork7@gmail.com>
2022-07-10 17:28:20 -0700

    add memo

--- 08-encryption/02-bcrypt-practice/memo.md
+++ 08-encryption/02-bcrypt-practice/memo.md
@@ -4,3 +4,10 @@
   https://www.udemy.com/course/nodejs-the-complete-guide/learn/lecture/11954350#overview
 
 # memo
+
+- password should be encrypted for security
+- install `bcryptjs`
+- encrypt password
+  ```
+  const hashedPassword = await bcrypt.hash(password, 12);
+  ```
--- 08-encryption/02-bcrypt-practice/src/server.js
+++ 08-encryption/02-bcrypt-practice/src/server.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const express = require("express");
 
 const app = express();
@@ -5,8 +6,36 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-app.post("/signup", (req, res) => {
-  res.json(req.body);
+let user = { email: "", password: "" };
+
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  user = { email, hashedPassword };
+
+  res.json(user);
+});
+
+app.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  console.log("user", user);
+  console.log(`email, ${email}`);
+  console.log(`password, ${password}`);
+
+  if (email === user.email && password === user.password) {
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    res.json({ email, hashedPassword });
+  } else {
+    next(new Error("Login failed: No User Found"));
+  }
+});
+
+app.use((error, req, res, next) => {
+  console.log(error.message);
+  res.json({ message: error.message });
 });
 
 app.listen(PORT, () => {
